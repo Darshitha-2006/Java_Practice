d5eda1f7d25b6a4769200c533d4d8a081ebcd610
Darshitha <darshtech06@gmail.com>
2024-06-16 10:18:18 +0530

    4th commit made

--- simple.java
+++ simple.java
@@ -1,5 +1,7 @@
 public class simple{
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("wahh!!");
+        System.out.println("diff cmd is beautiful!!");
     }
 }
