21f280be56e0b1f940e706211be0e4d000dccca2
Darshitha <darshtech06@gmail.com>
2024-06-23 16:20:46 +0530

    linkedList is palindrome or not?

---
+++ Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(7);
+        list.insert(8);
+
+        list.show();
+
+    }
+}
